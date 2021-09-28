1f133afba15e97004d72764be2204df80b5ea355
panoramine <camila.c.a.salles@gmail.com>
2021-09-27 19:01:41 -0700

    added POST route to receive the form submission

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -13,6 +16,11 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
@@ -20,6 +28,10 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -34,3 +46,14 @@ app.get("/urls.json", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  let chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+  let charsArray = chars.split("");
+  let tinyUrl = "";
+  for (let i = 0; i < 6; i ++) {
+    let randomIndex = Math.floor(Math.random() * 37);
+    tinyUrl += charsArray[randomIndex];
+  }
+  return tinyUrl;
+}
