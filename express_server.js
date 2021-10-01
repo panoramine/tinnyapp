e899e27c132a4fc816c282a7300dbfaef8f5eaf3
panoramine <camila.c.a.salles@gmail.com>
2021-09-30 17:45:27 -0700

    refactored and linted express_server

--- express_server.js
+++ express_server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
-const morgan = require("morgan")
+const PORT = 8080;
+const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
 const bcrypt = require('bcryptjs');
-const { use } = require("chai");
 const helper = require("./helpers");
 const generateRandomString = helper.generateRandomString;
 const findUserByEmail = helper.findUserByEmail;
@@ -23,12 +22,12 @@ app.use(morgan("tiny"));
 
 const urlDatabase = {
   b6UTxQ: {
-      longURL: "https://www.tsn.ca",
-      userID: "aJ48lW"
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW"
   },
   i3BoGr: {
-      longURL: "https://www.google.ca",
-      userID: "aJ48lW"
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW"
   }
 };
 
@@ -41,10 +40,18 @@ const users = {
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  if (req.session.user_id) {
+    res.redirect("/urls");
+  }
+
+  res.redirect("/login");
 });
 
 app.get("/register", (req, res) => {
+  if (req.session.user_id) {
+    res.redirect("/urls");
+  }
+
   res.render("register");
 });
 
@@ -91,7 +98,7 @@ app.post("/urls/new", (req, res) => {
 
 app.post("/urls/:shortUrl/redirect", (req, res) => {
   if (!req.session.user_id) {
-    return res.status(400).send("400: you need to be logged in to edit urls");
+    return res.status(400).send("400: you must be logged in to edit urls");
   }
 
   const userUrlDatabase = createUserUrlDatabase(urlDatabase, req.session.user_id);
@@ -106,7 +113,7 @@ app.post("/urls/:shortUrl/redirect", (req, res) => {
 
 app.post("/urls/:shortUrl/delete", (req, res) => {
   if (!req.session.user_id) {
-    return res.status(400).send("400: you need to be logged in to delete urls");
+    return res.status(400).send("400: you must be logged in to delete urls");
   }
 
   const userUrlDatabase = createUserUrlDatabase(urlDatabase, req.session.user_id);
@@ -135,8 +142,7 @@ app.post("/login", (req, res) => {
   }
 
   const user = findUserByEmail(email, users);
-  console.log(users)
-  console.log(user)
+  
   if (!user) {
     return res.status(403).send("403: e-mail cannot be found, <a href='/login'>back to login</a>");
   }
@@ -145,11 +151,15 @@ app.post("/login", (req, res) => {
     return res.status(403).send("403: password is not a match, <a href='/login'>back to login</a>");
   }
 
-  req.session.user_id = user.id
+  req.session.user_id = user.id;
   res.redirect("/urls");
 });
 
 app.get("/login", (req, res) => {
+  if (req.session.user_id) {
+    res.redirect("/urls");
+  }
+
   res.render("login");
 });
 
@@ -167,10 +177,6 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/urls", (req, res) => {
   if (!req.session.user_id) {
     return res.status(400).send("400: you must be <a href='/login'>logged in</a> to have access to urls");
@@ -186,30 +192,26 @@ app.get("/urls/new", (req, res) => {
   const templateVars = { user: users[req.session.user_id] };
 
   if (!templateVars.user) {
-    return res.redirect("/login")
+    return res.redirect("/login");
   }
 
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.session.user_id] };
-  
   if (!req.session.user_id) {
-    return res.status(400).send("400: you need to be logged in to have access to urls");
+    return res.status(400).send("400: you must be logged in to have access to urls");
   }
-
+  
   const userUrlDatabase = createUserUrlDatabase(urlDatabase, req.session.user_id);
-
+  
   if (!userUrlDatabase[req.params.shortURL]) {
     return res.status(400).send(`400: ${req.params.shortURL} is not one of your short URLs`);
   }
+  
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.session.user_id] };
 
   res.render("urls_show", templateVars);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
